9805ca3c0069df26a60f985b3b100d36a141d4b0
Alexander <allgud83@gmail.com>
2021-07-17 12:51:42 +0300

    Все три слайдера

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,26 +5,27 @@ import SwiperCore, { Navigation, Pagination } from "swiper/core";
 let sliders = document.querySelectorAll('.swiper-container')
 
 const slider = () => {
-
-  if(window.innerWidth < 768 && sliders[1].dataset.mobile === 'false'){
-    SwiperCore.use([Navigation, Pagination])
-    let mobileSlider = new Swiper(sliders[1], {
-    slidesPerView: 1.19,
-    slideClass: 'slide',
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+  sliders.forEach( item => {
+    if(window.innerWidth < 768 && item.dataset.mobile === 'false'){
+      SwiperCore.use([Navigation, Pagination])
+      let mobileSlider = new Swiper(item, {
+      slidesPerView: 1.19,
+      slideClass: 'slide',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      }
+    })
+    item.dataset.mobile = 'true'
+   }
+   window.addEventListener('resize', () => {
+    if(innerWidth < 768){
+      slider()
+    }else {
+      item.dataset.mobile = 'false'
     }
+    })
   })
-  sliders[1].dataset.mobile = 'true'
- }
- window.addEventListener('resize', () => {
-  if(innerWidth < 768){
-    slider()
-  }else {
-    sliders[1].dataset.mobile = 'false'
-  }
-})
 }
 
 slider()
